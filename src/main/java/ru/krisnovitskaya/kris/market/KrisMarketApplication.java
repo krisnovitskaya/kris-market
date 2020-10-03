94bc8ec39dd792f4a19f20a4ee1b95c49b74ba31
Kris Novitskaya <voitage@yandex.ru>
2020-10-03 11:22:29 +0300

    part1 new_order

--- src/main/java/ru/krisnovitskaya/kris/market/KrisMarketApplication.java
+++ src/main/java/ru/krisnovitskaya/kris/market/KrisMarketApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KrisMarketApplication {
-
+	// Домашнее задание:
+	// 1. На странице корзины делаем кнопку оформить заказ, нас перебрасывает на страницу,
+	// где показываются товары из корзины, без возможности их менять
+	// 2. На странице оформления заказа указывается имя клиента, его номер телефона, и адрес
+	// доставки, по нажатию на кнопку оформить, заказ должен быть сохранен в базу данных
 	public static void main(String[] args) {
 		SpringApplication.run(KrisMarketApplication.class, args);
 	}
--- src/main/java/ru/krisnovitskaya/kris/market/controllers/OrderController.java
+++ src/main/java/ru/krisnovitskaya/kris/market/controllers/OrderController.java
@@ -7,16 +7,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.krisnovitskaya.kris.market.services.OrderService;
+import ru.krisnovitskaya.kris.market.utils.Cart;
 
 @Controller
 @RequestMapping("/orders")
 @AllArgsConstructor
 public class OrderController {
     private OrderService orderService;
+    private Cart cart;
+
 
     @GetMapping
     public String firstRequest(Model model) {
         model.addAttribute("orders", orderService.findAll());
         return "orders";
     }
+
+    @GetMapping("/new")
+    public String newOrder(Model model){
+        return "new_order";
+    }
+
+
 }
