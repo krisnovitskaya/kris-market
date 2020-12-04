59eaed5e8ad2df6622828821954b59565f8fa2a6
krisnovitskaya <voitage@yandex.ru>
2020-12-04 16:13:08 +0300

    security + simple repository

---
+++ src/test/java/ru/krisnovitskaya/kris/market/RepositoryTest.java
@@ -0,0 +1,28 @@
+package ru.krisnovitskaya.kris.market;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import ru.krisnovitskaya.kris.market.dto.ProfileDto;
+import ru.krisnovitskaya.kris.market.entities.Profile;
+import ru.krisnovitskaya.kris.market.repositories.ProfileRepository;
+
+@DataJpaTest
+@ActiveProfiles("h2")
+public class RepositoryTest {
+
+    @Autowired
+    ProfileRepository profileRepository;
+
+    @Test
+    public void checkFindProfileByUsername(){
+        Profile profile = profileRepository.findProfileByUsername("user1");
+        Assertions.assertNotNull(profile);
+        Assertions.assertEquals(1980, profile.getBirthYear());
+        System.out.println(new ProfileDto(profile));
+    }
+}
