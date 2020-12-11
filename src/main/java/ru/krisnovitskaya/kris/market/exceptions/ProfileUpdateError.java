79e59e7f0f88e4ee8552572594eb147b48d54435
krisnovitskaya <voitage@yandex.ru>
2020-12-11 13:31:28 +0300

    prof validate

--- src/main/java/ru/krisnovitskaya/kris/market/exceptions/ProfileUpdateError.java
+++ src/main/java/ru/krisnovitskaya/kris/market/exceptions/ProfileUpdateError.java
@@ -11,25 +11,18 @@ import java.util.stream.Collectors;
 
 public class ProfileUpdateError {
     private int status;
-    //private List<String> messages;
-    private String messages;
+    private List<String> messages;
     private Date timestamp;
 
     public ProfileUpdateError(List<ObjectError> errors) {
         this.status = HttpStatus.BAD_REQUEST.value();
-        StringBuilder sb = new StringBuilder();
-        for (ObjectError error : errors) {
-            sb.append(error.getDefaultMessage());
-            sb.append("; ");
-        }
-        //this.messages = errors.stream().map(e -> e.getDefaultMessage()).collect(Collectors.toList());
+        this.messages = errors.stream().map(e -> e.getDefaultMessage()).collect(Collectors.toList());
         this.timestamp = new Date();
     }
 
     public ProfileUpdateError(String message) {
         this.status = HttpStatus.BAD_REQUEST.value();
-        //this.messages = new ArrayList<>(Arrays.asList(message));
-        this.messages = message;
+        this.messages = new ArrayList<>(Arrays.asList(message));
         this.timestamp = new Date();
     }
 }
